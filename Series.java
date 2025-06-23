706c6ed55049d0579a62385fd6edd210c14d1c18
Rashmi-Ranjan-Prusty <rashmiranjanp052@gmail.com>
2025-06-23 16:13:04 +0530

    i have done a new program which find the series of numbers divisible by 11

---
+++ Series.java
@@ -0,0 +1,11 @@
+public class Series {
+    public static void main(String[] args) {
+    int num=1;
+    while (num<100) {
+        if (num%11==0) {
+            System.out.println(num);
+        }
+        num++;
+    }
+   }
+}
