6cdfed4d7c56ba9d5fccc2af7289b32f6bc1b1dd
Rashmi-Ranjan-Prusty <rashmiranjanp052@gmail.com>
2025-06-24 23:16:08 +0530

    Add Sum.java that prints sum of given number

--- Sum.java
+++ Sum.java
@@ -1,3 +1,11 @@
 public class Sum {
-    
+        public static void main(String[] args) {
+        int num=97587, sum=0;
+        while (num!=0) {
+            int rem=num%10;
+            sum=sum+rem;
+            num=num/10;
+        }
+        System.out.println(sum);
+    }
 }
