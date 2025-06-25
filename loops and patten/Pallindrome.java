52224451457e1778fa83c0a9928ce0878d00cf5f
Rashmi-Ranjan-Prusty <rashmiranjanp052@gmail.com>
2025-06-25 23:35:36 +0530

    Upadte Pallindrome

--- loops and patten/Pallindrome.java
+++ loops and patten/Pallindrome.java
@@ -1,3 +1,19 @@
+import java.util.*;
 public class Pallindrome {
+    public static void main(String[] args) {
+        int test, rem, pall=0;
+        Scanner sc = new Scanner(System.in);  
+        int num=sc.nextInt();
+        for ( test=num; test!=0; test/=10) {
+            rem=test%10;
+            pall=pall*10+rem;
+        }
+        System.out.println(pall);
+        if (pall==num) {
+            System.out.println("pallindrome");
+        } else {
+            System.out.println("not");
+        }
+    }
     
 }
