3f856f73a63047755bad00905b3c2cb49a31a128
Rashmi-Ranjan-Prusty <rashmiranjanp052@gmail.com>
2025-06-25 23:30:51 +0530

    Created Factorial.java

--- loops and patten/Factorial.java
+++ loops and patten/Factorial.java
@@ -1,3 +1,10 @@
 public class Factorial {
-    
+    public static void main(String[] args) {
+        int num=4, fact=1;
+        while (num!=0) {
+            fact=fact*num;
+            num--;
+        }
+        System.out.println(fact);
+    }
 }
